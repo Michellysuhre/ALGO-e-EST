28d1946b1a471753bcf7a20ea3efcbd8c8fd4d2b
Michelly Aparecida Suhre <michellysuhre@gmail.com>
2025-03-20 14:41:42 -0400

    COMENTARIOS

---
+++ index.py
@@ -0,0 +1,15 @@
+# # isso é um comentario 
+# # print("mensagem") # print = escreval o ctrl ; faz virar um comentario
+# idade=6
+# altura=0.50 # em ingles nao se usa , troca por .
+# peso=5.950
+# nome="francisco" # para declarar carcteres ainda tem que adicionar ""
+# print(idade, altura, peso, nome) # não precisa declarar as variaveis como idade:inteiro
+# #caracte= string(str)
+# #inteiro=int
+#
+nome=input("insira seu nome")
+idade=int(input("insira sua idade"))
+peso=float(input("insira seu peso"))
+print(f"{nome} tem {idade} anos e pesa {peso} kg") 
+# quando esta fora de {} sera mostrado para o usuario
