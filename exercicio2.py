662f46c7f7fb093031b5fe9382815756e1ea41c4
Michelly Aparecida Suhre <michellysuhre@gmail.com>
2025-03-20 14:42:22 -0400

    EXERCICIOS

---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+nome=input("insira seu nome") # input = caractere
+idade=int(input("insira sua idade"))
+print(f"Olá, {nome}! você tem {idade} anos. ")
---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+num1=int(input("insira o primeiro numero "))
+num2=int(input("insira o segundo numero "))
+soma=num1+num2
+subtracao=num1-num2
+multiplicacao=num1*num2
+divisao=num1/num2
+print(f"o resultado da soma é {soma}, da subtração é {subtracao}, da multiplicação é {multiplicacao} e da divisão é {divisao}")
